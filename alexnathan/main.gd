1964152d2c3f6c89f2b382adbe882667ab5f6294
metalicmagic45 <alexkhangnguyen@gmail.com>
2025-04-13 14:22:29 -0700

    Added Quit Button

--- alexnathan/main.gd
+++ alexnathan/main.gd
@@ -13,7 +13,8 @@ func _on_play_button_button_down() -> void:
 	get_tree().change_scene_to_file("res://zone_1.tscn")
 
 func _on_exit_button_button_down() -> void:
-	print(20)
+	get_tree().quit()
+
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
