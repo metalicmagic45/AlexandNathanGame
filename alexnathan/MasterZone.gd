0607cbba7b7d7cf6136a39f5e216c683613fe16f
metalicmagic45 <alexkhangnguyen@gmail.com>
2025-04-18 18:00:57 -0700

    Mergeconflicts

--- alexnathan/MasterZone.gd
+++ alexnathan/MasterZone.gd
@@ -9,6 +9,8 @@ extends Node2D
 func _input(event): 
 	if event.is_action_pressed("uicancel"):
 		toggle_pause_menu()
+	if event.is_action_pressed("ToggleInvetory"):
+		get_tree().change_scene_to_file("res://inventory_ui.tscn")
 func toggle_pause_menu():
 	PausePanel.visible = !PausePanel.visible
 
