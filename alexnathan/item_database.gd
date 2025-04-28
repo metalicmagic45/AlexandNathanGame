49ccd1baf68c46c4befda040d4c39d24167c8447
Betrum <nathanaschober@gmail.com>
2025-04-28 16:19:56 -0700

    !!!!Don't fetch before commiting!!!!, updated weapon data and change weapon img file pathing

--- alexnathan/item_database.gd
+++ alexnathan/item_database.gd
@@ -3,27 +3,68 @@ extends Node
 var items = {}
 
 func _ready() -> void:
-	items["colt1911"] = {
-		"name": "Colt 1911",
-		"sprite": preload("res://Pictures/1911colt.png"),		
-	}
-	items["berettaM9"] = {
-		"name": "Beretta M9",
-		"sprite": preload("res://Pictures/m9.png")
+	items["Glock17"] = {
+		"name": "Glock 17",
+		"sprite": preload("res://Pictures/Weapons/g17.png")
 	}
 	items["HIPower"] = {
 		"name": "Hi-Power",
-		"sprite": preload("res://Pictures/HiPower.png")
+		"sprite": preload("res://Pictures/Weapons/HiPower.png")
 	}
-	items["Glock17"] = {
-		"name": "Glock 17",
-		"sprite": preload("res://Pictures/g17.png")
+	items["berettaM9"] = {
+		"name": "Beretta M9",
+		"sprite": preload("res://Pictures/Weapons/m9.png")
+	}
+	items["colt1911"] = {
+		"name": "Colt 1911",
+		"sprite": preload("res://Pictures/Weapons/1911colt.png"),		
+	}
+	items["ColtPython"] = {
+		"name": "Colt Python",
+		"sprite": preload("res://Pictures/Weapons/coltpython.png"),		
+	}
+	items["S&WModel29"] = {
+		"name": "S&W Model 29",
+		"sprite": preload("res://Pictures/Weapons/s&w model 29.png"),		
+	}
+	items["RugerMini-14"] = {
+		"name": "Ruger Mini-14",
+		"sprite": preload("res://Pictures/Weapons/Ruger Mini-14.png"),		
+	}
+	items["Remington700"] = {
+		"name": "Remington 700",
+		"sprite": preload("res://Pictures/Weapons/remington 700.png"),		
+	}
+	items["Marlin4445"] = {
+		"name": "Marlin 4445",
+		"sprite": preload("res://Pictures/Weapons/marlin 444s.png"),		
+	}
+	items["Rem870"] = {
+		"name": "Rem 870",
+		"sprite": preload("res://Pictures/Weapons/remmington870.png"),		
 	}
 	items["m14"] = {
 		"name": "M14",
-		"sprite": preload("res://Pictures/m14.png")
+		"sprite": preload("res://Pictures/Weapons/m14.png")
+	}
+	items["RugerN0.1"] = {
+		"name": "Ruger No.1",
+		"sprite": preload("res://Pictures/Weapons/Ruger Mini-14.png"),		
+	}
+	items["WeatherbyMarkV"] = {
+		"name": "Weatherby Mark V",
+		"sprite": preload("res://Pictures/Weapons/weatherby Mark V.png"),		
 	}
 	items["axe"] = {
 		"name": "Axe",
-		"sprite": preload("res://Pictures/axe.png")
+		"sprite": preload("res://Pictures/Weapons/axe.png")
 	}
+	items["Machete"] = {
+		"name": "Machete",
+		"sprite": preload("res://Pictures/Weapons/machete.png"),		
+	}
+	
+	#items[""] = {
+		#"name": "",
+		#"sprite": preload("res://Pictures/"),		
+	#}
