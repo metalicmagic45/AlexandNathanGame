9cd73c182ccf7093d949ed3f3f78133c00d44db1
metalicmagic45 <alexkhangnguyen@gmail.com>
2025-05-30 18:44:12 -0700

    Added "Return"

--- alexnathan/load.gd
+++ alexnathan/load.gd
@@ -3,16 +3,6 @@ extends Control
 @onready var PausePanel = $Panel
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main.tscn")
 func toggle_pause_menu():
@@ -20,3 +10,5 @@ func toggle_pause_menu():
 func _input(event): 
 	if event.is_action_pressed("uicancel"):
 		toggle_pause_menu()
+func _on_return_button_down() -> void:
+	get_tree().change_scene_to_file("res://MasterScene.tscn")
