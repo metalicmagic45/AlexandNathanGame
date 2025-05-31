b445c16a283e1694943a8eac543657f88edc658c
metalicmagic45 <alexkhangnguyen@gmail.com>
2025-05-31 13:58:29 -0700

    Button Tracking

--- alexnathan/load.gd
+++ alexnathan/load.gd
@@ -1,8 +1,11 @@
 extends Control
 
 @onready var PausePanel = $Panel
-
-
+@onready var title1 = $HBoxContainer/Save1/CenterContainer/VBoxContainer/Title
+@onready var title2 = $HBoxContainer/Save2/CenterContainer/VBoxContainer/Title
+@onready var title3 = $HBoxContainer/Save3/CenterContainer/VBoxContainer/Title
+@onready var title4 = $HBoxContainer/Save4/CenterContainer/VBoxContainer/Title
+@onready var title5 = $HBoxContainer/Save5/CenterContainer/VBoxContainer/Title
 func _on_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main.tscn")
 func toggle_pause_menu():
