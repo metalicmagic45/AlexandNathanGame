44d6c58215940b5c1026cd7dab8d5d6d4dae0c7f
metalicmagic45 <alexkhangnguyen@gmail.com>
2025-06-23 21:42:56 -0700

    UI Framework for Notes Improved

---
+++ alexnathan/journal.gd
@@ -0,0 +1,24 @@
+extends Node
+
+var journal_entries = {
+	"MissionReport": {
+		"title": "Operation Hollow Butcher",
+		"status": "Incomplete",
+		"description": """
+		You have been ordered to investigate the disappearance of Regan Hilstad, secure occult and supernatural items possessed by the cult, 
+		gather intelligence of the cult, assess threat level, and report findings for potential escalation.
+		"""
+	}
+		
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+func get_journal():
+	return journal_entries
--- alexnathan/notes.gd
+++ alexnathan/notes.gd
@@ -1,14 +1,22 @@
 extends Control
 
+@onready var Itemlist = $TextureRect/VBoxContainer/HBoxContainer/ItemList
+
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("NotesToggle"):
 		get_tree().change_scene_to_file("res://MasterScene.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	populate_list()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func populate_list():
+	var item_list = Itemlist
+	item_list.clear()
+	var notes = Journal.get_journal()
+	for item_name in notes:
+		item_list.add_item(item_name)
