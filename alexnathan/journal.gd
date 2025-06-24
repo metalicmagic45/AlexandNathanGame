879e8154158ca73e5b564175da6d220fe4b9deb0
metalicmagic45 <alexkhangnguyen@gmail.com>
2025-06-23 21:56:44 -0700

    Notes UI Improvement
    Added the ability to select and display journal entries.

--- alexnathan/journal.gd
+++ alexnathan/journal.gd
@@ -5,8 +5,9 @@ var journal_entries = {
 		"title": "Operation Hollow Butcher",
 		"status": "Incomplete",
 		"description": """
-		You have been ordered to investigate the disappearance of Regan Hilstad, secure occult and supernatural items possessed by the cult, 
-		gather intelligence of the cult, assess threat level, and report findings for potential escalation.
+		You have been ordered to investigate the disappearance of Regan Hilstad, 
+		secure occult and supernatural items possessed by the cult, gather intelligence of the cult, 
+		assess threat level, and report findings for potential escalation.
 		"""
 	}
 		
--- alexnathan/notes.gd
+++ alexnathan/notes.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var Itemlist = $TextureRect/VBoxContainer/HBoxContainer/ItemList
+@onready var label = $TextureRect/VBoxContainer/HBoxContainer/Panel/Label
 
 
 func _input(event: InputEvent) -> void:
@@ -17,6 +18,17 @@ func _process(delta: float) -> void:
 func populate_list():
 	var item_list = Itemlist
 	item_list.clear()
+
 	var notes = Journal.get_journal()
-	for item_name in notes:
-		item_list.add_item(item_name)
+	for note_id in notes:
+		var note = notes[note_id]
+		item_list.add_item(note["title"])
+		item_list.set_item_metadata(item_list.item_count - 1, note_id)
+
+
+
+func _on_item_list_item_clicked(index: int, at_position: Vector2, mouse_button_index: int) -> void:
+	var notes = Journal.get_journal()
+	var note_id = Itemlist.get_item_metadata(index)
+	var note = notes[note_id]
+	label.text = note["description"]
