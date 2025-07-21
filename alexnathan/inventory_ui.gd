b12f984c82f051fece2ca142fa8920e907dde4d8
metalicmagic45 <alexkhangnguyen@gmail.com>
2025-07-21 13:58:11 -0700

    Quick Fi

--- alexnathan/inventory_ui.gd
+++ alexnathan/inventory_ui.gd
@@ -1,13 +1,17 @@
 extends Control	
 @onready var grid_container: GridContainer = $VBoxContainer/GridContainer
 @onready var itemcontainerscene = preload("res://inventory_slot.tscn")
+@onready var itemlist = $VBoxContainer/Control/HBoxContainer/ItemList
+@onready var sprite = $VBoxContainer/Control/HBoxContainer/Control/VBoxContainer/Panel/CenterContainer/TextureRect
 
 func _input(event):
 	if event.is_action_pressed("ToggleInvetory"):
 		get_tree().change_scene_to_file("res://MasterScene.tscn")
+	if event.is_action_pressed("uicancel"):
+		itemlist.release_focus()
 
 func reset():
-	for child in grid_container.get_children():
+	for child in itemlist.get_children():
 		child.queue_free()
 func add_item(item : String):
 	var inventoryslot = itemcontainerscene.instantiate()
@@ -20,5 +24,17 @@ func load_player_inventory(player : String):
 	for item : String in data: 
 		add_item(item)
 func _ready():
-	reset()
-	#load_player_inventory("William Clark")
+	populate_list()
+func populate_list():
+	itemlist.clear()
+	var item_ids = Playerinventory.get_inventory("William Clark")
+	for i in item_ids:
+		var item = ItemDatabase.items.get(i)
+		if item.has("name"):
+			itemlist.add_item(item["name"])
+
+
+func _on_item_list_item_clicked(index: int, at_position: Vector2, mouse_button_index: int) -> void:
+	var item_name = itemlist.get_item_text(index)
+	if ItemDatabase.items.has(item_name) and ItemDatabase.items[item_name].has("sprite"):
+		sprite.texture = ItemDatabase.items[item_name]["sprite"]
