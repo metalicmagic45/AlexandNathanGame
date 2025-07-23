a0d4bedf6c1ecfd04e2a05f25d0be0c5ddbeb922
metalicmagic45 <alexkhangnguyen@gmail.com>
2025-07-23 09:52:10 -0700

    Basic Framework for Battlemap

--- alexnathan/tile.gd
+++ alexnathan/tile.gd
@@ -9,4 +9,9 @@ var tile_size: Vector2 = Vector2(64, 64) # default fallback
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("uicancel"):
 		tile.release_focus()
-	
+func _draw():
+	var outline_offset = Vector2(-32, -32)
+	var outline_rect = Rect2(outline_offset, tile_size)
+	var thickness = 1.0
+
+	draw_rect(outline_rect, Color.WHITE, false, thickness)
