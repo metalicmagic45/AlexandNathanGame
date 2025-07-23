374167ea759b251442658a2b10932d5ba766f419
metalicmagic45 <alexkhangnguyen@gmail.com>
2025-07-22 23:00:19 -0700

    Basic Progress on Combat System

--- alexnathan/tile.gd
+++ alexnathan/tile.gd
@@ -1,4 +1,4 @@
-extends ColorRect
+extends Node2D
 
 @onready var spritetexture = $Sprite2D
 @onready var tile = $Button
@@ -6,14 +6,6 @@ var grid_pos: Vector2i
 
 var tile_size: Vector2 = Vector2(64, 64) # default fallback
 
-func _ready():
-	queue_redraw()
-func _draw():
-	# Black fill
-	draw_rect(Rect2(Vector2.ZERO, tile_size), Color.BLACK, true)
-	
-	# White border
-	draw_rect(Rect2(Vector2.ZERO, tile_size), Color.WHITE, false)
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("uicancel"):
 		tile.release_focus()
